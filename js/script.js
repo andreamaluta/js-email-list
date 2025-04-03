e83ad3d2b472fc4c9dbf73caea18b41890aff671
andreamaluta <andreamaluta@gmail.com>
2025-04-03 14:30:23 +0200

    [UPD] created the cycle to created the 10 random mail

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const list = document.getElementById('lists');
 
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
-    console.log(resp.data.response);
-})
+
+
+for(let i=0; i<10; i++){
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
+        console.log(resp.data.response)
+    }) 
+}
