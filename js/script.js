ab9b28c8f645a685f9705eeeabe3522b6ed4d5ef
andreamaluta <andreamaluta@gmail.com>
2025-04-03 14:34:12 +0200

    [UPD] put the random mail inside the html of ul

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 const list = document.getElementById('lists');
 
 
-
 for(let i=0; i<10; i++){
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
-        console.log(resp.data.response)
+        list.innerHTML += `<li>${resp.data.response}</li>`;
     }) 
 }
